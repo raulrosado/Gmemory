704242a9958830ce6f6fede570ad52930178612c
Raul Remedios Rosado <raulrosado91@gmail.com>
2022-05-05 08:56:18 -0400

    Add: puntos de los view pages

--- app/src/main/java/com/serproteam/gmemory/ui/fragment/OpcionesJuego.kt
+++ app/src/main/java/com/serproteam/gmemory/ui/fragment/OpcionesJuego.kt
@@ -1,17 +1,26 @@
 package com.serproteam.gmemory.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.serproteam.gmemory.Adapter.SlidePagerAdapterSlider
 import com.serproteam.gmemory.R
 import com.serproteam.gmemory.core.ReplaceFragment
 import com.serproteam.gmemory.databinding.FragmentHomeBinding
 import com.serproteam.gmemory.databinding.FragmentOpcionesJuegoBinding
 import com.serproteam.gmemory.ui.viewmodel.SliderViewModel
 import com.serproteam.pideloapp.core.TinyDB
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +32,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [OpcionesJuego.newInstance] factory method to
  * create an instance of this fragment.
  */
+@AndroidEntryPoint
 class OpcionesJuego : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -30,12 +40,15 @@ class OpcionesJuego : Fragment() {
 
     private var _binding: FragmentOpcionesJuegoBinding? = null
     private val binding get() = _binding!!
-    lateinit var tinyDB: TinyDB
-    val replaceFragment = ReplaceFragment()
+//    @Inject lateinit var tinyDB: TinyDB
+    @Inject lateinit var replaceFragment : ReplaceFragment
+
+    var sliderPager: ViewPager? = null
+    var pagerAdapter: PagerAdapter? = null
 
     var current_position = 0
     var fragmentList: ArrayList<Fragment>? = null
-
+    private val slideViewModel: SliderViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,12 +64,58 @@ class OpcionesJuego : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentOpcionesJuegoBinding.inflate(inflater, container, false)
-        tinyDB = TinyDB(requireContext())
         val fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
 
+        fragmentList = ArrayList<Fragment>()
+        fragmentList = slideViewModel.getSliderData()
+        Log.v("GmemoryLog", "tamaño lista: " + fragmentList?.size)
+
+        pagerAdapter =
+            SlidePagerAdapterSlider(requireActivity().getSupportFragmentManager(), fragmentList)
+        binding.sliderPager.adapter = pagerAdapter
+
+        preparetDots(current_position++)
+        binding.sliderPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(i: Int, v: Float, i1: Int) {}
+            override fun onPageSelected(i: Int) {
+                if (current_position > 4) {
+                    current_position = 0
+                }
+                preparetDots(i)
+            }
+
+            override fun onPageScrollStateChanged(i: Int) {}
+        })
 
 
         return binding.root
+    }
+
+    fun preparetDots(currentSlidePosition: Int) {
+        if (binding.dostContainer.getChildCount() > 0) {
+            binding.dostContainer.removeAllViews()
+        }
+        val dots: Array<ImageView?> = arrayOfNulls<ImageView>(fragmentList!!.size)
+        for (i in fragmentList!!.indices) {
+            dots[i] = ImageView(requireContext())
+            if (i == currentSlidePosition) dots[i]?.setImageDrawable(
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.dost
+                )
+            ) else dots[i]?.setImageDrawable(
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.inactive_dost
+                )
+            )
+            val layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.setMargins(4, 0, 4, 0)
+            binding.dostContainer.addView(dots[i], layoutParams)
+        }
     }
 
     companion object {
